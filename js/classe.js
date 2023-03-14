bdc7d94bc59dde1d33dfcb56103765904efa0a99
Iuri <iuri-rdutra2@educar.rs.gov.br>
2023-03-14 17:36:50 -0300

    terceiro commit

---
+++ js/classe.js
@@ -0,0 +1,96 @@
+class Ator
+{
+    constructor(id,nome){
+        this.nome=nome;
+        this.id=id
+    }
+}
+
+class Diretor
+{
+    constructor(id,nome){
+        this.nome=nome;
+        this.id=id
+    }
+}
+
+
+class Filme{
+    constructor(id,titulo,ano,genero,cartaz,sinopse,duracao,direcao,elenco,classificacao,avaliacao){
+        this.titulo=titulo;
+        this.ano=ano;
+        this.genero=genero;
+        this.duracao=duracao;
+        this.sinopse=sinopse;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.elenco=elenco;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+    }
+
+    getCard = async () => {
+        let card = document.createElement("div");
+        card.setAttribute("class", "card");
+        let imgCartaz = document.createElement("img");
+        imgCartaz.setAttribute("class", "card-img-topz");
+        imgCartaz.setAttribute("src", this.cartaz);
+        let cardBody = document.createElement("div");
+        cardBody.setAttribute("class", "card-body");
+        let hCardTitle = document.createElement("h5");
+        hCardTitle.setAttribute("class", "card-title");
+        let divDetalhes = document.createElement("div");
+        divDetalhes.setAttribute(
+          "style",
+          "display:flex; justify-content:space-aroud;"
+        );
+        let divGenero = document.createElement("div");
+        divGenero.setAttribute("style", "flex-grow:1;");
+        let divAnoProducao = document.createElement("div");
+        divAnoProducao.setAttribute("style", "flex-grow:1;");
+        let divClassificacao = document.createElement("div");
+        divClassificacao.setAttribute("style", "flex-grow:1;");
+        hCardTitle.appendChild(document.createTextNode(this.titulo));
+        divGenero.appendChild(document.createTextNode(this.genero));
+        divAnoProducao.appendChild(document.createTextNode(this.ano));
+        divClassificacao.appendChild(document.createTextNode(this.classificacao));
+        divDetalhes.appendChild(divGenero);
+        divDetalhes.appendChild(divAnoProducao);
+        divDetalhes.appendChild(divClassificacao);
+        card.appendChild(imgCartaz);
+        card.appendChild(cardBody);
+        cardBody.appendChild(hCardTitle);
+        cardBody.appendChild(divDetalhes);
+        return card;
+      };
+      
+}
+
+/*
+btnBuscarFilme.onclick
+=
+async () => {
+if(inputBuscarFilme.value.length > 0) {
+let filmes = new Array();
+fetch("http://www.omdbapi.com/?apikey=ed5e5ad5&s="+inputBuscarFilme.value)
+.then((resp) => resp.json())
+.then((resp) => {
+resp.Search.forEach((item)=>{ console.log(item);
+let filme-new Filme(
+item.imdbID,
+item.Title,
+item.Year,
+null,
+null,
+item.Poster,
+null,
+null,
+null,
+null,
+null
+filmes.push(filme);
+www
+});
+return false;
+}
+}*/
--- js/script.js
+++ js/script.js
@@ -1,14 +1,51 @@
-let inputBuscarFilmes = document.querySelector("input-buscar-filmes");
-let btnBuscarFilmes = document.querySelector("btn-buscar-filmes");
+let inputBuscarFilmes = document.querySelector("#input-buscar-filmes");
+let btnBuscarFilmes = document.querySelector("#btn-buscar-filmes");
 
 
-btnBuscarFilmes.onclick = () =>{
-    if(inputBuscarFilmes.value.length > 0){
-        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=a6bbbd1f" +inputBuscarFilmes.value, {mode:"cors"})
-        .then((resp)=> resp.json())
-        .then((resp)=> {
-            console.log(resp);
-        })
-    }
-    return false;
+btnBuscarFilmes.onclick = () => {
+  
+  if (inputBuscarFilmes.value.length > 0) {
+    let filmes = new Array();
+    fetch("https://www.omdbapi.com/?i=tt3896198&apikey=1b5322a&s=" + inputBuscarFilmes.value)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        resp.Search.forEach((item)=>{ 
+          console.log(item);
+          let filme=new Filme(
+            item.imdbID,
+            item.Title,
+            item.Year,
+            null,
+            item.Poster,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+          
+          );
+          filmes.push(filme);
+          
+      });
+      listarFilmes(filmes);
+      
+    })
+    
+  }
+          
+  return false;
+}
+
+let listarFilmes = async(filmes) =>{
+  let listaFilmes = await document.querySelector("#lista-filmes");
+ 
+  listaFilmes.innerHTML = "";
+  
+  console.log(listaFilmes);
+  if(filmes.length > 0){
+    filmes.forEach(async(filme) => {
+      listaFilmes.appendChild(await filme.getCard());
+    })
+  }
 }
